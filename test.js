0ffd2b516b940e12996cda7953600ed54c0fd3bf
AmmAr MouRad <amrmrd111spcl@hotmail.com>
2018-05-12 22:59:17 +0300

    fix filter

--- test.js
+++ test.js
@@ -1,15 +1,16 @@
 const Validall = require('./');
 
-let user = { name: [1] };
+let user = { name: 'ammar', age: 33 };
 let schema = new Validall.Schema({
-  name: String,
-  users: 'string[]',
-  permissions: [{ model: String, services: 'string[]' }]
+  name: String
+}, {
+  filter: true,
+  strict: false
 });
 
-// console.log(user);
+console.log(user);
 let state = schema.test(user);
 
 console.log(state);
 console.log(schema.error);
-// console.log(user);
+console.log(user);
