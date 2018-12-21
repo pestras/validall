1348f581d4e10223e89609cec2eb6728867ba96a
ammar6885 <amrmrdb51@gmail.com>
2018-12-21 15:31:19 +0300

    v3.0.4

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./index");
-console.log(index_1.validate("-1", { $type: 'string' }));
+console.log(index_1.validate("-1", { $is: 'notEmpty' }));
