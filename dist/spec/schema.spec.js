130bfca997d170acad5546b9deae8d0488dc4adf
pestras <admin@pestras.com>
2021-07-07 09:54:48 +0300

    fix types

--- dist/spec/schema.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const __1 = require("../");
-describe("testing Schema Validations", () => {
-    it("should not through on empty schema", () => {
-        expect(() => { new __1.Validall({}); }).not.toThrow();
-    });
-});
-//# sourceMappingURL=schema.spec.js.map
