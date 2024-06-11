b83ea74b1302dd912e5e6c46220fb698b8dc288e
Ammar Mourad <amrmrdb51@gmail.com>
2024-06-11 10:40:51 +0300

    schema repo

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,12 +1,40 @@
 import { SchemaContext } from "./ctx";
 import { ValidallError } from "./errors";
 import { validate } from "./operations";
-import { IsDate, IsString } from "./types";
 import { ObjectSchema } from "./types/object-schema";
 
 export class Schema<T extends object> {
+  private static repo = new Map<string, Schema<any>>;
 
-  constructor(readonly schema: ObjectSchema<T>) { }
+  readonly name!: string;
+  readonly schema!: ObjectSchema<T>;
+
+  constructor(schema: ObjectSchema<T>)
+  constructor(name: string, schema: ObjectSchema<T>)
+  constructor(arg1: string | ObjectSchema<T>, arg2?: ObjectSchema<T>) {
+    if (typeof arg1 === 'string') {
+      this.name = arg1;
+      if (arg2)
+        this.schema = arg2;
+    } else {
+      this.name = '';
+      this.schema = arg1
+    }
+
+    if (!this.schema)
+      throw "validation schema is required";
+
+    if (this.name) {
+      if (Schema.repo.has(this.name))
+        throw `validation schema name '${this.name}' already exists`;
+
+      Schema.repo.set(this.name, this);
+    }
+  }
+
+  static Get<T extends object>(name: string): Schema<T> | null {
+    return Schema.repo.get(name) ?? null;
+  }
 
   validate(input: T, prefix?: string): ValidallError | undefined {
 
@@ -27,7 +55,7 @@ export class Schema<T extends object> {
     } catch (error) {
       if (prefix)
         throw error;
-      
+
       return error;
     }
   }
